5f00420b71331b89ae72ffa2f643b2209def7a57
tm00r <timurka.gareev@gmail.com>
2021-11-27 12:08:46 +0100

    WIP: is_blocked() was implemented

--- du5.py
+++ du5.py
@@ -1,4 +1,4 @@
-class GamePlayground:
+class Playground:
 
     def __init__(self, width, height) -> None:
         self.height = height
@@ -18,30 +18,35 @@ class GamePlayground:
     def add_block(self, x: int, y: int) -> None:
         for i, j in zip(range(x-1, x+2), range(y-1, y+2)):
             if 0 <= i <= self.width - 1:
-                self.blocked_pos.append(self.playground[y][i])
                 self.playground[y][i] = False
                 self.blocked_row_pos.append(self.playground[y][i])
             if 0 <= j <= self.height - 1:
-                self.blocked_pos.append(self.playground[j][x])
                 self.playground[j][x] = False
                 self.blocked_col_pos.append(self.playground[j][x])
         self.draw_field()
 
-    # def is_blocked(self, x: int, y: int) -> bool:
+    def is_blocked(self, x: int, y: int) -> bool:
+        return any(
+            0 <= i <= self.width - 1
+            and not self.playground[y][i]
+            or 0 <= j <= self.height - 1
+            and not self.playground[j][x]
+            for i, j in zip(range(x - 1, x + 2), range(y - 1, y + 2))
+        )
 
 
     # def is_full(self) -> bool:
 
 
 
-class GameParticipant:
+class Participant:
 
     def __init__(self) -> None:
         pass
 
 
 
-class Player(GameParticipant):
+class Player(Participant):
 
     def __init__(self) -> None:
         super().__init__()
@@ -49,9 +54,12 @@ class Player(GameParticipant):
 
 
 def main():
-    field = GamePlayground(10, 10)
+    field = Playground(10, 10)
     # field.add_block(3, 3)
-    field.add_block(9, 0)
+    if not field.is_blocked(9, 0):
+        field.add_block(9, 0)
+    else:
+        print('DUUUUUUUUUUDE')
 
 if __name__=='__main__':
     main()
